feceee71adcb2fc0263775759486a5e9ad20e120
owalkz <jjaraorie@gmail.com>
2024-02-21 14:59:01 +0300

    Initial Commit

--- Python/user_input.py
+++ Python/user_input.py
@@ -1,1 +1,5 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
 
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
